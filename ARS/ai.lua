4b5b3e8ced9a137c4dc65fd46a4f67fe6b5ff06b
AlmiriQ <50090571+AlmiriQ@users.noreply.github.com>
2022-01-15 22:10:12 +0700

    Add files via upload

--- ARS/ai.lua
+++ ARS/ai.lua
@@ -1,1 +1,30 @@
- 
+return function(Maria, message)
+   local id = message.author.id
+   print("/ai", id)
+   local function level_to_string(n)
+      if     n >= 128 then return "Maria"
+      elseif n >= 64  then return "God"
+      elseif n >= 32  then return "Overprotected"
+      elseif n >= 16  then return "Protection"
+      elseif n >= 8   then return "Admin"
+      elseif n >= 4   then return "System"
+      elseif n >= 2   then return "User"
+      elseif n >= 1   then return "Application"
+      else                 return "File"
+      end
+   end
+   local function getDefaultLevel(user)
+      if user.bot then return 1 end
+      return 2
+   end
+   message.channel:send("Your level is: " .. 
+      level_to_string(tonumber(
+         Maria.user_db:exec(
+            (([[
+               INSERT OR IGNORE INTO user VALUES(%s, %s);
+               SELECT level FROM user WHERE id == %s;
+            ]])):format(id, getDefaultLevel(message.member), id)
+         )[1][1]
+      ))
+   )
+end
--- main.lua
+++ main.lua
@@ -1,7 +1,9 @@
+require"functional/functional"
 local discordia = require"discordia"
 local BotBuilder = require"functional/bot builder"(discordia, require"functional/logger")
 local recursive_scan = require"functional/rscan"(require"fs")
-local Emitter = require('core').Emitter
+local Emitter = require"core".Emitter
+local sql = require "sqlite3"
 
 
 local Maria_Arusu_builder = BotBuilder()
@@ -17,42 +19,117 @@ function Maria_Arusu_builder.messageCreate(message)
    if message.author.id == Maria_Arusu.client.user.id then return end
    local message_text = message.content:lower()
    if message_text:gsub("Н", "н"):gsub("Я", "я") == "ня" or message_text == "nya" then
-      if Maria_Arusu.connection == nil then
-         if message.member and message.member.voiceChannel:join() then
-            Maria_Arusu.connection = message.member.voiceChannel:join()
-         end
-      end
-      coroutine.wrap(function()
-         if Maria_Arusu.connection then
-            Maria_Arusu.connection:playFFmpeg('sound/Maria_Meow_' .. tostring(math.random(1, 8)) .. '.opus')
-         end
-      end)()
-      message:addReaction"🐱"
-      message.channel:send("Nya, " .. message.author.mentionString .. " <3"):addReaction"🐱"
+      Maria_Arusu.musicPlayer:emit("nya", message)
+      return
    elseif message.content == "AEsir" or message.content == "Æsir" then
-      coroutine.wrap(function()
-         if Maria_Arusu.connection then
-            Maria_Arusu.connection:playFFmpeg('sound/Chaos.mp3')
-         end
-      end)()
+      Maria_Arusu.musicPlayer:emit("aesir", message)
+      return
    end
-   local prefixes = { "a:", "A:", "ARS::" }
+   if message.content:startswith(Maria_Arusu.prefix) then
+      print(pcall(function()
+         local command = Maria_Arusu.cache:get(message.content)
+         if not command then
+            Maria_Arusu.cache:push(message.content, require("ARS"..message.content))
+            command = Maria_Arusu.cache:get(message.content)
+         end
+         command(Maria_Arusu, message)
+      end))
+   end
 end
 
 
 Maria_Arusu = Maria_Arusu_builder:build()
 Maria_Arusu.connection = nil
+Maria_Arusu.user_db = sql.open("database/users.db")
+Maria_Arusu.cache = {
+   keys = {},
+   data = {},
+   maxv = 10,
+   push = function(cache, key, data)
+      cache:collect()
+      if cache.keys[key] then return end
+      cache.keys[key] = {
+         usages = 0,
+         cdata = data
+      }
+      table.insert(cache.data, cache.keys[key])
+   end,
+   get = function(cache, key)
+      local re_value = cache.keys[key]
+      if re_value then
+         re_value.usages = re_value.usages + 1
+         return re_value.cdata
+      end
+   end,
+   clear = function(cache)
+      cache.data = {}
+      cache.keys = {}
+      collectgarbage"collect"
+   end,
+   collect = function(cache)
+      if #cache.data < cache.maxv then return end
+      local c_data = {}
+      local sum_usages = 0
+      for _, dt in ipairs(cache.data) do sum_usages = sum_usages + dt.usages end
+      for k, dt in ipairs(cache.keys) do
+         if dt.usages / sum_usages > (1 / cache.maxv / 1.5) then
+            c_data[k] = dt
+         end
+      end
+      cache:clear()
+      for k, v in pairs(c_data) do
+         cache:push(k, v)
+      end
+   end
+}
+Maria_Arusu.user_db:exec[[
+CREATE TABLE IF NOT EXISTS user(id INTEGER PRIMARY KEY, level UNSIGNED TINYINT);
+INSERT OR IGNORE INTO user VALUES
+   ("928003461684023346", 128), 
+   ("415453831724400641", 64),
+   ("822440442460110858", 64);
+]]
 Maria_Arusu.musicPlayer = Emitter:new()
+Maria_Arusu.prefix = "/"
+Maria_Arusu.bitwise = require"bitwise"
 
+function Maria_Arusu.musicPlayer.connect(message)
+   if message.member and message.member.voiceChannel then
+      if Maria_Arusu.connection == nil then
+         Maria_Arusu.connection = message.member.voiceChannel:join()
+      elseif Maria_Arusu.connection.channel.id ~= message.member.voiceChannel.id then
+         Maria_Arusu.connection:close()
+         Maria_Arusu.connection = message.member.voiceChannel:join()
+      end
+   end
+end
+
+Maria_Arusu.musicPlayer:on("nya", function(message)
+   Maria_Arusu.musicPlayer.connect(message)
+   pcall(coroutine.wrap(function()
+      Maria_Arusu.connection:playFFmpeg('sound/Maria_Meow_' .. tostring(math.random(1, 8)) .. '.opus')
+   end))
+   message:addReaction"🐱"
+   message.channel:send("Nya, " .. message.author.mentionString .. " <3"):addReaction"🐱"
+end)
+
+Maria_Arusu.musicPlayer:on("aesir", function(message)
+   Maria_Arusu.musicPlayer.connect(message)
+   pcall(coroutine.wrap(function()
+      Maria_Arusu.connection:playFFmpeg('sound/Chaos.mp3')
+   end))
+end)
 
 Maria_Arusu.commands = {}
 for _, file in pairs(recursive_scan"ARS") do
-   --print(file:sub(5, file))
+   print(file:sub(5, #file - 4))
 end
 
 
-local token_file = io.open"token"
-local token = token_file:read"*a"
-token_file:close()
-Maria_Arusu:run(token)
-token = nil
+do
+   local token_file = io.open"token"
+   local token = token_file:read"*a"
+   token_file:close()
+   Maria_Arusu:run(token)
+   token = nil
+end
