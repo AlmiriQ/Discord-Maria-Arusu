55f22d81fb7350f5d9cada657aeb18a1b7d7fb14
AlmiriQ <50090571+AlmiriQ@users.noreply.github.com>
2022-01-20 15:33:06 +0700

    Add files via upload

---
+++ ARS/userdata/file/infinite-nya.lua
@@ -0,0 +1,6 @@
+return function(env)
+   if not env.Maria then return -1 end
+   if not env.message then return -1 end
+   if env.user.level < 2 then env.message.channel:send"You do not have enough permissions to execute this file!" return end 
+   env.Maria.musicPlayer:emit("infinite-nya-start", env.message)
+end
